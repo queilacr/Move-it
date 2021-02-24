a9b0b297568905fa1935ca58ce96c252e7668253
queilacr <queilacostar@gmail.com>
2021-02-23 22:15:17 -0300

    dia1

---
+++ moveit/src/App.tsx
@@ -0,0 +1,17 @@
+
+import { ExperienceBar } from './components/ExperienceBar';
+
+import './styles/global.css';
+
+
+function App() {
+  return (
+    
+    <div className='container'>
+      <ExperienceBar />
+
+    </div>
+  );
+}
+
+export default App;
